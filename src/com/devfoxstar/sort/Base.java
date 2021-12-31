defde8b61d82aebcaa3894b826dcd796ba09bbe9
devfoxstar <devfoxstar@gmail.com>
2022-01-01 00:32:28 +0900

    Sort Test

--- src/com/devfoxstar/sort/Base.java
+++ src/com/devfoxstar/sort/Base.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class Base {
 
     @Test
-    public void sortTest1() {
+    public void sortIntArray() {
         final int[] numbers = {-4, 3, -1, 5, 0, 1};
         Arrays.sort(numbers);
 
@@ -16,12 +16,21 @@ public class Base {
     }
 
     @Test
-    public void sortTest2() {
+    public void sortIntArrayEqual() {
         final int[] numbers = {-2, -1, 0, 1, 5, -3};
         final int[] expected = {-3, -2, -1, 0, 1, 5};
 
         Arrays.sort(numbers);
         assertArrayEquals(expected, numbers);
+    }
+
+    @Test
+    public void sortObjectArrayEqual() {
+        final String[] strings = {"b", "d", "a", "c", "e"};
+        final String[] expected = {"a", "b", "c", "d", "e"};
+
+        Arrays.sort(strings);
+        assertArrayEquals(expected, strings);
     }
 
 }
