86c8f48a2b6aa5748d8a3bacdfe7810589d5b625
devfoxstar <devfoxstar@gmail.com>
2022-02-13 20:33:32 +0900

    Queue

---
+++ src/com/devfoxstar/datastructure/queue/JavaQueue.java
@@ -0,0 +1,29 @@
+package com.devfoxstar.datastructure.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class JavaQueue {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<String>();
+
+        //enqueue
+        queue.offer("A");
+        queue.add("B");
+        queue.add("C");
+        queue.add("D");
+        queue.add("E");
+        System.out.println(queue.peek());       //A
+
+        //dequeue
+        queue.poll();
+        System.out.println(queue.peek());       //B
+
+        //remove
+        queue.remove("D");
+        System.out.println(queue);              //[B, C, E]
+
+        //element
+        System.out.println(queue.element());    //B
+    }
+}
